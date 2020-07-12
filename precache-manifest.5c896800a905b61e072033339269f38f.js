7e17599d80d01ad21263119cebe6588dc7031eff
Arunky122 <arunky122@gmail.com>
2020-07-12 18:26:32 +0530

    Updates

---
+++ precache-manifest.5c896800a905b61e072033339269f38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fb0e8d3f5c63f9eab821e5ce8124d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a534450c0aee7e876d86",
+    "url": "/static/css/main.18bedff2.chunk.css"
+  },
+  {
+    "revision": "d9b47e4f947a67226901",
+    "url": "/static/js/2.7692c723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7692c723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a534450c0aee7e876d86",
+    "url": "/static/js/main.f162aa36.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  }
+]);
